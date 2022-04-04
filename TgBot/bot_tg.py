07160df6e63eeb25ef063047acd6acfad3a0764b
Uropb1221 <94992413+Uropb1221@users.noreply.github.com>
2022-04-04 23:28:16 +0300

    Update bot_tg.py

--- TgBot/bot_tg.py
+++ TgBot/bot_tg.py
@@ -22,7 +22,7 @@ WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
+WEBAPP_PORT = os.getenv('PORT', default=5000)
 
 
 async def on_startup(dispatcher):
